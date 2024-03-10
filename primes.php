1d41bb2046b96c940d6a69ca4c46c448242764c2
Sega93 <157649905+Sega93@users.noreply.github.com>
2024-03-10 23:06:27 +0100

    Add files via upload

---
+++ primes.php
@@ -0,0 +1,51 @@
+<?php
+
+// Check einer Zahl ob es sich um eine Primzahl handelt
+function primeCheck($num) {
+  if ($num == 1) {
+    return false; // Eingabe 1 = keine Primzahl
+  }
+  for ($i = 2; $i < $num; $i++) {
+    if ($num % $i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Erstellfunktion der Tabelle
+function primeList($input) {
+  $totalPrimes = 0;
+  $totalLines = 1; // =1 damit Zeile 2 den Wert 2 hat
+  echo "Zeile\n";
+  for ($n = 1; $n <= $input; $n++) {
+    if ($n == 2) {
+      printf("%5d", 1); //printf um Werte rechtsbündig auszugeben
+    }
+    if (primeCheck($n)) {
+      printf("%7d", $n);
+      $totalPrimes ++;
+      if ($totalPrimes % 10 == 0) {
+        //Zeilenumbruch und Ausgabe neuer Zeilennummer
+        echo "\n";
+        printf ("%5d", $totalLines);
+      }
+      // separater $totalLines +Counter damit erst bei Benutzung einer neuen Zeile der Wert erhöht wird (nicht schon bei 10 wie zB. im Fall $input = 29)
+      if ($totalPrimes % 10 == 1) {
+        $totalLines++;
+      }
+    }
+  }
+  $totalLines -= 1; // $totalLines -1 um dem Hilfs-Startwert 1 entgegenzuwirken bei der Ausgabe 
+  // Ausgabefeld
+  echo "\n\n";
+  echo "Anzahl der Zeilen: $totalLines\n";
+  echo "Anzahl der Primzahlen: $totalPrimes\n";
+}
+
+// User-Inputfeld um Ausgabebereich der Liste anzugeben
+$input = readline("Gib eine Nummer ein bis zu der die Primzahlen gelistet werden sollen: ");
+echo "\n";
+primeList($input);
+
+?>
