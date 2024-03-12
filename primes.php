28e22922031040d3c99c0ed01f7d40f2821f75e7
Sega93 <157649905+Sega93@users.noreply.github.com>
2024-03-12 18:58:28 +0100

    Add files via upload

--- primes.php
+++ primes.php
@@ -2,11 +2,11 @@
 
 // Check einer Zahl ob es sich um eine Primzahl handelt
 function primeCheck($num) {
-  if ($num == 1) {
-    return false; // Eingabe 1 = keine Primzahl
+  if ($num == 1) {                    // Eingabe 1 = keine Primzahl
+    return false;                       
   }
-  for ($i = 2; $i < $num; $i++) {
-    if ($num % $i == 0) {
+  for ($i = 2; $i < $num; $i++) {     // bei $num = 2 -> "2 < 2" ist nicht "true", geht daher nicht in den for-Loop, und wird so direkt "true"
+    if ($num % $i == 0) {         
       return false;
     }
   }
@@ -16,27 +16,19 @@ function primeCheck($num) {
 // Erstellfunktion der Tabelle
 function primeList($input) {
   $totalPrimes = 0;
-  $totalLines = 1; // Hilfswert 1 für Zeile "1"
+  $totalLines = 0;
   
-  for ($n = 1; $n <= $input; $n++) {
-    if ($n == 2) {
-      echo "Zeile $totalLines: \t";
-    }
-    if (primeCheck($n)) {  // hier vllt noch n switch einbauen mit case primeCheck($n) und case primeCheck($n) && $totalPrimes % 10 == 1, damit Zeilen erst kommen wenn 11 Primes da sind
-      echo "$n\t";
-      $totalPrimes ++;
-      if ($totalPrimes % 10 == 0) {
-        //Zeilenumbruch und Ausgabe neuer Zeilennummer
+  for ($n = 1; $n <= $input; $n++) {  // jede Zahl bis $input erreicht ist wird auf Primzahl-Eigenschaft überprüft
+    if (primeCheck($n)) {
+      $totalPrimes ++;                // Primzahlcounter +1 wenn primeCheck($n) "true"
+      if ($totalPrimes % 10 == 1) {   // Check ob Primzahlencounter 11 erreicht hat, dann Umbruch und Zeilencounter +1
+        $totalLines++;
         echo "\n";
         echo "Zeile $totalLines: \t";
       }
-      // separater $totalLines +Counter damit erst bei Benutzung einer neuen Zeile der Wert erhöht wird (nicht schon bei 10 wie zB. im Fall $input = 29)
-      if ($totalPrimes % 10 == 1) {
-        $totalLines++;
-      }
+      echo "$n\t";                    // Primzahl wird gelistet
     }
   }
-  $totalLines -= 1; // Entgegenwirken zum Hilfswert aus Line 19
 
   // Ausgabefeld
   echo "\n\n";
